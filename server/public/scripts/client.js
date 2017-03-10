3e5c0d47a35b47fe6cda851c00f6504c1b905ba3
samvelie <sam.velie@gmail.com>
2017-03-09 20:21:42 -0600

    figured out 1.6.2 jsonp error fix

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,24 @@
 var myApp = angular.module('myApp', ['ngRoute']);
 
-myApp.config(['$routeProvider', function($routeProvider) {
+myApp.config(['$routeProvider', '$sceDelegateProvider', function($routeProvider, $sceDelegateProvider) {
 
-    $routeProvider
-        .when('/home', {
-            templateUrl: '/views/templates/home.html',
-            controller: 'HomeController',
-            controllerAs: 'hc'
-        })
-        .when('/other', {
-            templateUrl: '/views/templates/other.html',
-            controller: 'OtherController',
-            controllerAs: 'oc'
-        })
-        .otherwise({
-            redirectTo: 'home'
-        });
+  $sceDelegateProvider.resourceUrlWhitelist([
+    'self',
+    'http://api.petfinder.com/**'
+  ]);
+
+  $routeProvider
+  .when('/home', {
+    templateUrl: '/views/templates/home.html',
+    controller: 'HomeController',
+    controllerAs: 'hc'
+  })
+  .when('/other', {
+    templateUrl: '/views/templates/other.html',
+    controller: 'OtherController',
+    controllerAs: 'oc'
+  })
+  .otherwise({
+    redirectTo: 'home'
+  });
 }]);
--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -1,8 +1,40 @@
-myApp.controller('HomeController', function() {
+myApp.controller('HomeController', ['$http', function($http) {
   console.log('home controller running');
 
   var self = this;
 
+  self.petObject = {};
+
   self.message = "Welcome to the Home View";
 
-});
+  var api_key = 'c131370934be7916dbc03da9ee196061';
+  var baseURL = 'http://api.petfinder.com/';
+
+  self.getRandomPet = function(){
+    // create a URL
+    var query = baseURL + 'pet.getRandom';
+    query += '?key=' + api_key;
+    query += '&animal=pig';
+    query += '&output=basic';
+    query += '&format=json';
+    // query += '&callback=JSON_CALLBACK';
+
+    console.log('api query', query);
+
+
+    //encode that URL
+    var request = encodeURI(query);
+
+    console.log('encodedURI', request);
+
+
+    //make ajax request
+    $http.jsonp(request, {jsonpCallbackParam: 'callback'}).then(function(response){
+      console.log('response', response.data.petfinder.pet);
+      self.petObject= response.data.petfinder.pet;
+    });
+
+    // when request is complete, put info on DOM
+  };
+
+}]);
